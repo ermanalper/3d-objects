2faaf2ed93109e704a0ed80cb83e6eab0039c9f2
ermanalper <ermanalper6@gmail.com>
2026-01-08 22:54:11 +0300

    sdl window

--- 3d-objects.c
+++ 3d-objects.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-int main() 
+
+#define WIDTH 900
+#define HEIGHT 600
+
+int main()
 {
-	printf("hello");
-	return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
+    SDL_Window* pwindow = SDL_CreateWindow(
+        "3D Objects",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        0
+    );
 
+    if (!pwindow) {
+        printf("Window Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+	
+    SDL_Surface* psurface = SDL_GetWindowSurface(pwindow);
+    SDL_UpdateWindowSurface(pwindow);
+	
+
+    SDL_Event e;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                running = 0;
+        }
+    }
+
+    SDL_DestroyWindow(pwindow);
+    SDL_Quit();
+    return 0;
 }
-
 
